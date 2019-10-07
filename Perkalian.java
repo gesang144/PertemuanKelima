a8f502ea43243618aae8c8820e9fa70fad69aba9
gesang144 <44185920+gesang144@users.noreply.github.com>
2019-10-08 06:26:10 +0700

    Acc

---
+++ Perkalian.java
@@ -0,0 +1,20 @@
+public class Perkalian{
+
+    public static void main(String[] args) {
+
+        int  angkaPertama;
+        int angkaKedua;
+        int hasil;
+        
+        angkaPertama = 18;
+       angkaKedua = 2;
+       hasil= angkaPertama * angkaKedua;
+       System.out.println(" hasil perkalian dari kedua angka tersebut adalah " + hasil);
+      
+
+     
+      
+
+
+    }
+}
